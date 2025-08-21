b1e673601a8825ed1667f6521aace50dfc8b3844
Alex Bierwagen <me@alexb.io>
2025-08-20 19:48:31 -0700

    initial mac config

--- hosts/ab-m4mbp/default.nix
+++ hosts/ab-m4mbp/default.nix
@@ -1,9 +1,28 @@
 {
-  inputs,
   pkgs,
   ...
 }:
-
+let
+  user = "alex";
+in
 {
+  imports = [ ];
 
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+    };
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [
+    utm
+  ];
+
+  system = {
+    primaryUser = user;
+  };
 }
