b42c89f4527132a3b31ab3fe1ed1adbd324fa55d
Alex Bierwagen <me@alexb.io>
2025-08-31 09:01:38 -0700

    add casks

--- hosts/ab-m4mbp/default.nix
+++ hosts/ab-m4mbp/default.nix
@@ -1,58 +1,66 @@
-  {
-    pkgs,
-    inputs,
-    ...
-  }:
-  let
-    user = "alex";
-    weekly = {
-      Weekday = 6;
-    };
-  in
-  {
-    imports = [
-      inputs.home-manager.darwinModules.home-manager
-      {
-        users.users.alex.home = /Users/alex;
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          users.${user} = ../../home.nix;
-        };
-      }
-
-    ];
-
-    nix = {
-      gc = {
-        automatic = true;
-        interval = weekly;
+{
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  user = "alex";
+  weekly = {
+    Weekday = 6;
+  };
+in
+{
+  imports = [
+    inputs.home-manager.darwinModules.home-manager
+    {
+      users.users.alex.home = /Users/alex;
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        users.${user} = ../../home.nix;
       };
-      optimise = {
-        automatic = true;
-        interval = weekly;
-      };
-      extraOptions = ''
-        experimental-features = nix-command flakes
-      '';
-    };
+    }
 
-    environment.systemPackages = with pkgs; [
-      nixfmt
-      nil
-      just
-      utm
-      deploy-rs
+  ];
+
+  nix = {
+    gc = {
+      automatic = true;
+      interval = weekly;
+    };
+    optimise = {
+      automatic = true;
+      interval = weekly;
+    };
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [
+    nixfmt
+    nil
+    just
+    utm
+    deploy-rs
+  ];
+
+  programs.nh = {
+    enable = true;
+    flake = "/Users/alex/Development/01_nix";
+  };
+
+  homebrew = {
+    enable = true;
+    casks = [
+      "raycast"
+      "publii"
     ];
-    
-    programs.nh {
-      enable = true;
-      flake = "/Users/alex/Development/01_nix";
-    };
+  };
 
-    system = {
-      primaryUser = user;
-      # initial version for setting backwards incompatibility
-      stateVersion = 6;
-    };
-  }
+  system = {
+    primaryUser = user;
+    # initial version for setting backwards incompatibility
+    stateVersion = 6;
+  };
+}
