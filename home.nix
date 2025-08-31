ad892f850304a2829dde51bfb8cc4dc4a05bec75
Alex Bierwagen <me@alexb.io>
2025-08-31 15:07:12 -0700

    add starship prompt

--- home.nix
+++ home.nix
@@ -72,6 +72,62 @@
     enableZshIntegration = true;
   };
 
+  programs.zoxide.enable = true;
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      # copying from https://starship.rs/presets/pure-preset
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_state"
+        "$git_status"
+        "$cmd_duration"
+        "$line_break"
+        "$python"
+        "$character"
+      ];
+      directory.style = "blue";
+      character = {
+        success_symbol = "[λ](purple)";
+        error_symbol = "[λ](red)";
+      };
+      git_branch = {
+        format = "[$branch]($style)";
+        style = "bright-black";
+      };
+      git_status = {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+        style = "cyan";
+        conflicted = "​";
+        untracked = "​";
+        modified = "​";
+        staged = "​";
+        renamed = "​";
+        deleted = "​";
+        stashed = "≡";
+      };
+      git_state = {
+        format = "\([$state( $progress_current/$progress_total)]($style)\) ";
+        style = "bright-black";
+      };
+      cmd_duration = {
+        format = "[$duration]($style)";
+        style = "yellow";
+      };
+      python = {
+        format = "[$virtualenv]($style)";
+        style = "bright-black";
+      };
+    };
+  };
+
   programs.nh = {
     enable = true;
     darwinFlake = "/Users/alex/Development/01_nix/flake.nix";
