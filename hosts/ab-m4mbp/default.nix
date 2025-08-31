fbe5a770d9a6575f2b0beeff1ac57b67e166a9c0
Alex Bierwagen <alex@bierwagen.dev>
2025-08-31 01:17:36 -0700

    add nh

--- hosts/ab-m4mbp/default.nix
+++ hosts/ab-m4mbp/default.nix
@@ -1,53 +1,58 @@
-{
-  pkgs,
-  inputs,
-  ...
-}:
-let
-  user = "alex";
-  weekly = {
-    Weekday = 6;
-  };
-in
-{
-  imports = [
-    inputs.home-manager.darwinModules.home-manager
-    {
-      users.users.alex.home = /Users/alex;
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        users.${user} = ../../home.nix;
+  {
+    pkgs,
+    inputs,
+    ...
+  }:
+  let
+    user = "alex";
+    weekly = {
+      Weekday = 6;
+    };
+  in
+  {
+    imports = [
+      inputs.home-manager.darwinModules.home-manager
+      {
+        users.users.alex.home = /Users/alex;
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          users.${user} = ../../home.nix;
+        };
+      }
+
+    ];
+
+    nix = {
+      gc = {
+        automatic = true;
+        interval = weekly;
       };
-    }
-
-  ];
-
-  nix = {
-    gc = {
-      automatic = true;
-      interval = weekly;
+      optimise = {
+        automatic = true;
+        interval = weekly;
+      };
+      extraOptions = ''
+        experimental-features = nix-command flakes
+      '';
     };
-    optimise = {
-      automatic = true;
-      interval = weekly;
+
+    environment.systemPackages = with pkgs; [
+      nixfmt
+      nil
+      just
+      utm
+      deploy-rs
+    ];
+    
+    programs.nh {
+      enable = true;
+      flake = "/Users/alex/Development/01_nix";
     };
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
 
-  environment.systemPackages = with pkgs; [
-    nixfmt
-    nil
-    just
-    utm
-    deploy-rs
-  ];
-
-  system = {
-    primaryUser = user;
-    # initial version for setting backwards incompatibility
-    stateVersion = 6;
-  };
-}
+    system = {
+      primaryUser = user;
+      # initial version for setting backwards incompatibility
+      stateVersion = 6;
+    };
+  }
