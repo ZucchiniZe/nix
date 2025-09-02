78c1407bce799ac20d6935abe29a509cb3e47db3
Alex Bierwagen <me@alexb.io>
2025-09-01 22:40:27 -0700

    add helix

--- home.nix
+++ home.nix
@@ -92,6 +92,52 @@
     # plugins = [ pkgs.vimPlugins.nvim-tree-lua ];
   };
 
+  programs.helix = {
+    enable = true;
+    extraPackages = [ pkgs.nixd ];
+    languages = {
+      languages = [
+        {
+          name = "nix";
+          auto-format = true;
+        }
+      ];
+      language-server.nixd = {
+        command = "nixd";
+        args = [ "--semantic-tokens=true" ];
+        config.nixd =
+          let
+            myFlake = "(builtins.getFlake toString /Users/alex/Developer/01_nix)";
+            darwinOpts = "${myFlake}.darwinConfigurations.ab-m4mbp.options";
+          in
+          {
+            formatting.command = "nixfmt";
+            nixpkgs.expr = "import ${myFlake}.inputs.nixpkgs { }";
+            options = {
+              darwin.expr = darwinOpts;
+              home-manager.expr = "${darwinOpts}.home-manager.users.type.getSubOptions []";
+            };
+          };
+      };
+    };
+    settings = {
+      theme = "penumbra+";
+      editor = {
+        line-number = "relative";
+        lsp.display-messages = true;
+      };
+      keys.normal = {
+        space.space = "file_picker";
+        space.w = ":w";
+        space.q = ":q";
+        esc = [
+          "collapse_selection"
+          "keep_primary_selection"
+        ];
+      };
+    };
+  };
+
   programs.eza = {
     enable = true;
     enableZshIntegration = true;
