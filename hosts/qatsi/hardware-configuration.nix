0c1bab9f5a7c09f8c55480245236d434ed20cbd6
Alex Bierwagen <me@alexb.io>
2025-09-23 22:25:37 -0700

    use disko fully

--- hosts/qatsi/hardware-configuration.nix
+++ hosts/qatsi/hardware-configuration.nix
@@ -23,24 +23,6 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/69dd3d87-2ffa-4a30-a834-95a884a03f14";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/BF7A-DFD0";
-    fsType = "vfat";
-    options = [
-      "fmask=0077"
-      "dmask=0077"
-    ];
-  };
-
-  swapDevices = [
-    { device = "/dev/disk/by-uuid/76110eba-4b8b-4dc6-930a-74dccdf9cf41"; }
-  ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
