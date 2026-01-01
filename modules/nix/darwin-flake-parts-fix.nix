31a61bc26175818c8ca2d394124d1d41820f91df
Alex Bierwagen <me@alexb.io>
2026-01-01 03:16:18 -0800

    learned the lesson that you can't have options and flake as both top level elements

---
+++ modules/nix/darwin-flake-parts-fix.nix
@@ -0,0 +1,21 @@
+{
+  inputs,
+  lib,
+  flake-parts-lib,
+  ...
+}:
+{
+
+  # https://github.com/Doc-Steve/dendritic-design-with-flake-parts/blob/main/modules/nix/flake-parts%20%5B%5D/darwinConfigurations-fix.nix
+  # currently there's no nix-darwin module for flake-parts,
+  # so we need to manually add the darwinConfigurations option.
+
+  options = {
+    flake = flake-parts-lib.mkSubmoduleOptions {
+      darwinConfigurations = lib.mkOption {
+        type = lib.types.lazyAttrsOf lib.types.raw;
+        default = { };
+      };
+    };
+  };
+}
--- modules/system/default/darwin.nix
+++ modules/system/default/darwin.nix
@@ -1,23 +1,8 @@
 {
   inputs,
-  lib,
-  flake-parts-lib,
   ...
 }:
 {
-
-  # https://github.com/Doc-Steve/dendritic-design-with-flake-parts/blob/main/modules/nix/flake-parts%20%5B%5D/darwinConfigurations-fix.nix
-  # currently there's no nix-darwin module for flake-parts,
-  # so we need to manually add the darwinConfigurations option.
-
-  options = {
-    flake = flake-parts-lib.mkSubmoduleOptions {
-      darwinConfigurations = lib.mkOption {
-        type = lib.types.lazyAttrsOf lib.types.raw;
-        default = { };
-      };
-    };
-  };
 
   flake-file.inputs = {
     nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-25.11-darwin";
@@ -27,7 +12,7 @@
     };
   };
 
-  flake.moduels.darwin.system-default = {
+  flake.modules.darwin.system-default = {
 
     nixpkgs.config.allowUnfree = true;
     system.stateVersion = 6;
