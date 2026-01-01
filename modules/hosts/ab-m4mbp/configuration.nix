715f7cc5a72712850d30c5b12c02917d95a9b06e
Alex Bierwagen <me@alexb.io>
2026-01-01 03:34:30 -0800

    cleanup + scaffold

--- modules/hosts/ab-m4mbp/configuration.nix
+++ modules/hosts/ab-m4mbp/configuration.nix
@@ -4,6 +4,7 @@
 }:
 {
   flake.darwinConfigurations = inputs.self.lib.mkDarwin "aarch64-darwin" "ab-m4mbp";
+
   flake.modules.darwin.ab-m4mbp = {
     imports = with inputs.self.modules.darwin; [
       system-default
