b09e7387a0db0a3e8e02261a0633f4fc242dc887
Alex Bierwagen <me@alexb.io>
2026-01-03 00:39:04 -0800

    home manager maybe

--- modules/users/alex.nix
+++ modules/users/alex.nix
@@ -3,28 +3,44 @@ let
   username = "alex";
   sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFPBRWC7uEA0ysNzYHMERozjdRuPUSD5kgwwmDH6DHmr";
 
-  flake.modules.nixos.${username} = {
-    users.users.${username} = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ];
-      openssh.authorizedKeys.keys = [ sshKey ];
+  flake.modules.homeManager.${username} =
+    { pkgs, ... }:
+    {
+      imports = with inputs.self.modules.homeManager; [ zsh ];
+
+      home.username = "${username}";
+      home.packages = with pkgs; [ ];
     };
 
-    # enable passwordless sudo
-    security.sudo.extraRules = [
-      {
-        users = [ username ];
-        commands = [
-          {
-            command = "ALL";
-            options = [ "NOPASSWD" ];
-          }
-        ];
-      }
-    ];
-  };
+  flake.modules.nixos.${username} =
+    { pkgs, ... }:
+    {
+      home-manager.users."${username}".imports = [ inputs.self.modules.homeManager."${username}" ];
+
+      users.users."${username}" = {
+        isNormalUser = true;
+
+        extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = [ sshKey ];
+        shell = pkgs.zsh;
+      };
+
+      programs.zsh.enable = true;
+
+      # enable passwordless sudo
+      security.sudo.extraRules = [
+        {
+          users = [ username ];
+          commands = [
+            {
+              command = "ALL";
+              options = [ "NOPASSWD" ];
+            }
+          ];
+        }
+      ];
+    };
   flake.modules.darwin.${username} = { };
-  flake.modules.homeManager.${username} = { };
 in
 {
   inherit flake;
