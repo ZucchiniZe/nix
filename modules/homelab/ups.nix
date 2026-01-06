fdee1319e465b87872f75bc383cf2dbdf2e3b0e3
Alex Bierwagen <me@alexb.io>
2026-01-05 21:27:00 -0800

    disable broken ups rn

--- modules/homelab/ups.nix
+++ modules/homelab/ups.nix
@@ -2,8 +2,13 @@
   flake.modules.nixos.ups = {
     power.ups = {
       enable = true;
-      mode = "netserver";
+      mode = "standalone"; # TODO: change to netserver
       openFirewall = true;
+      ups.noodle-factory = {
+        driver = "usbhid-ups";
+        port = "auto"; # for usb since `port` refers to a serial port
+        description = "the main ups for the noodle factory, connected via usb to qatsi";
+      };
     };
   };
 }
