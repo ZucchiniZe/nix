8f37e16ce62835215d7cc7dd8f7e7da3d0705bd4
Alex Bierwagen <me@alexb.io>
2026-01-06 02:03:52 -0800

    now with an actual hash

--- modules/homelab/caddy.nix
+++ modules/homelab/caddy.nix
@@ -19,6 +19,7 @@
           enable = true;
           package = pkgs.caddy.withPlugins {
             plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
+            hash = "sha256-dnhEjopeA0UiI+XVYHYpsjcEI6Y1Hacbi28hVKYQURg=";
           };
         };
       };
