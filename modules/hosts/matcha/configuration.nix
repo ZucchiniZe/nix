cafcdb4e45c3fd8f08cafe7aafbb3cef32be4061
Alex Bierwagen <me@alexb.io>
2026-02-02 20:15:38 -0800

    update whole flake + put matcha on nixpkgs-unstabel latest kernel

--- modules/hosts/matcha/configuration.nix
+++ modules/hosts/matcha/configuration.nix
@@ -2,49 +2,54 @@
 {
   flake.nixosConfigurations = inputs.self.lib.mkNixos "x86_64-linux" "matcha";
 
-  flake.modules.nixos.matcha = {
-    imports = with inputs.self.modules.nixos; [
-      limine-boot
-      system-desktop
-      niri
-      plasma
-      alex
-    ];
+  flake.modules.nixos.matcha =
+    { pkgs, ... }:
+    {
+      imports = with inputs.self.modules.nixos; [
+        limine-boot
+        system-desktop
+        niri
+        plasma
+        alex
+      ];
 
-    home-manager.users."alex" = {
-      imports = [ inputs.self.modules.homeManager.niri ];
+      # use the latest unstable kernel for the ABSOLUTE BLEEDING EDGE
+      boot.kernelPackages = pkgs.unstable.linuxPackages_latest;
 
-      programs.niri.settings = {
-        outputs."DP-1" = {
-          mode = {
-            height = 1440;
-            width = 3440;
-            refresh = 239.984;
+      home-manager.users."alex" = {
+        imports = [ inputs.self.modules.homeManager.niri ];
+
+        programs.niri.settings = {
+          outputs."DP-1" = {
+            mode = {
+              height = 1440;
+              width = 3440;
+              refresh = 239.984;
+            };
+            variable-refresh-rate = "on-demand";
+            focus-at-startup = true;
+            position = {
+              x = 1080;
+              y = 0;
+            };
           };
-          variable-refresh-rate = "on-demand";
-          focus-at-startup = true;
-          position = {
-            x = 1080;
-            y = 0;
-          };
-        };
 
-        outputs."HDMI-A-1" = {
-          transform.rotation = 90;
-          position = {
-            x = 0;
-            y = 0;
+          outputs."HDMI-A-1" = {
+            transform.rotation = 90;
+            position = {
+              x = 0;
+              y = 0;
+            };
           };
         };
       };
+
+      networking.hostName = "matcha";
+      networking.networkmanager.enable = true;
+
+      hardware.bluetooth.enable = true;
+
+      # dont delete - required for backwards compat checks
+      system.stateVersion = "25.11";
     };
-
-    networking.hostName = "matcha";
-    networking.networkmanager.enable = true;
-
-    hardware.bluetooth.enable = true;
-
-    # dont delete - required for backwards compat checks
-    system.stateVersion = "25.11";
-  };
 }
