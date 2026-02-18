11947ffa53bbf674a304c87ba5247834aa89ff64
Alex Bierwagen <me@alexb.io>
2026-02-18 00:22:07 -0800

    ope

--- modules/programs/1password.nix
+++ modules/programs/1password.nix
@@ -1,21 +1,22 @@
-{ pkgs, ... }:
 {
   flake.modules.homeManager._1password =
-    let
-      onePassPath =
-        if pkgs.stdenv.isLinux then
-          "~/.1password/agent.sock"
-        else
-          "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
-    in
+    { pkgs, ... }:
     {
-      programs.ssh = {
-        enable = true;
-        extraConfig = ''
-          		Host *
-             			IdentityAgent ${onePassPath}
-        '';
-      };
+      programs.ssh =
+        let
+          onePassPath =
+            if pkgs.stdenv.isLinux then
+              "~/.1password/agent.sock"
+            else
+              "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+        in
+        {
+          enable = true;
+          extraConfig = ''
+            		Host *
+               			IdentityAgent ${onePassPath}
+          '';
+        };
     };
 
   flake.modules.darwin._1password = {
