2cf2d8f769dcd7d24c75a4cf592e9e9fc5e0f742
Alex Bierwagen <me@alexb.io>
2026-07-02 21:16:30 -0700

    get the cached noctalia

--- flake.nix
+++ flake.nix
@@ -59,10 +59,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-26.05-darwin";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    noctalia = {
-      url = "github:noctalia-dev/noctalia";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    noctalia.url = "github:noctalia-dev/noctalia/cachix";
     packages = {
       url = "path:./packages";
       flake = false;
--- modules/programs/niri.nix
+++ modules/programs/niri.nix
@@ -10,8 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:noctalia-dev/noctalia/cachix";
     };
     silentSDDM = {
       url = "github:uiriansan/SilentSDDM";
