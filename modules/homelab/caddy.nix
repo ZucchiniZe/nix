2bd471a284d4dfde292191415993ebdfc3e27e6e
Alex Bierwagen <me@alexb.io>
2026-08-29 13:02:16 -0700

    update caddy hash

--- modules/homelab/caddy.nix
+++ modules/homelab/caddy.nix
@@ -26,8 +26,8 @@
         services.caddy = {
           enable = true;
           package = pkgs.caddy.withPlugins {
-            plugins = [ "github.com/caddy-dns/cloudflare@v0.2.3" ];
-            hash = "sha256-bL1cpMvDogD/pdVxGA8CAMEXazWpFDBiGBxG83SmXLA=";
+            plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
+            hash = "sha256-PWadA5qr/gR2qDcT8l8u1Xku7LM2HIfWTLOkzezCYy0=";
           };
 
           globalConfig = ''
